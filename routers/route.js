d980c565b9327a00058506b6e26efcc9d0bfe522
neerajsde <neerajprajapatise@gmail.com>
2024-05-19 22:09:36 +0530

    update login page

--- routers/route.js
+++ routers/route.js
@@ -10,18 +10,13 @@ const {deleteTask} = require('../controllers/deleteTask');
 const {editTask} = require('../controllers/editTask');
 
 router.post('/create/user', createNewUser);
-router.post('/login',auth, logIn);
+router.post('/login', logIn);
 router.post('/pendingTask/add', addPendingTask);
 router.post('/completedTask', addCompletedTask);
 router.delete('/deleteTask', deleteTask);
 router.put('/editTask', editTask);
 
-router.get('/user', auth, (req, res) => {
-    res.status(200).json({
-        sucess:true,
-        message:"user already loggedin"
-    })
-})
+router.get('/user', auth, logIn);
 
 
 module.exports = router;
