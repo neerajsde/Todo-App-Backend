46e134e5325ad80513052c6f4d471cf0efc8d7cb
neerajsde <neerajprajapatise@gmail.com>
2024-05-22 21:46:01 +0530

    update

--- controllers/sendOTP.js
+++ controllers/sendOTP.js
@@ -8,7 +8,7 @@ function generateOTP() {
     return crypto.randomInt(100000, 999999).toString();
 }
 
-exports.sendOTP = async(req, res) => {
+exports.sendOTP = async (req, res) => {
     const { email } = req.body;
 
     console.log("Request Body:", req.body); // Log the incoming request body
@@ -30,7 +30,7 @@ exports.sendOTP = async(req, res) => {
     }
 
     try {
-        const findEmail = await user.find({email:email});
+        const findEmail = await user.find({ email: email });
 
         if (findEmail.length === 0) {
             return res.status(400).json({
@@ -38,7 +38,7 @@ exports.sendOTP = async(req, res) => {
                 message: 'User not found'
             });
         }
-        console.log('User Find: ', findEmail);
+        console.log('User Found:', findEmail);
 
         const otp = generateOTP();
 
@@ -48,25 +48,25 @@ exports.sendOTP = async(req, res) => {
             secure: true,
             port: 465,
             auth: {
-                user: 'neeraj725086@gmail.com', // your Gmail address
-                pass: process.env.APP_PASSWORD // your Gmail password
+                user: process.env.EMAIL_USER, // your Gmail address from environment variable
+                pass: process.env.EMAIL_PASS // your Gmail password from environment variable
             }
         });
 
         const mailOptions = {
-            from: 'neeraj725086@gmail.com',
+            from: process.env.EMAIL_USER,
             to: email,
             subject: 'Todo App - Password Change Verification Code',
             text: `Dear User,
-    
-    We received a request to reset your password for your Todo App account. Please use the following One-Time Password (OTP) to proceed with changing your password:
-    
-    ${otp}
-    
-    This OTP is valid for the next 10 minutes. If you did not request a password change, please secure your account and contact our support team immediately.
-    
-    Best regards,
-    The Todo App Team`
+
+We received a request to reset your password for your Todo App account. Please use the following One-Time Password (OTP) to proceed with changing your password:
+
+${otp}
+
+This OTP is valid for the next 10 minutes. If you did not request a password change, please secure your account and contact our support team immediately.
+
+Best regards,
+The Todo App Team`
         };
 
         console.log("Mail Options:", mailOptions); // Log the mail options to ensure the email is set
@@ -74,7 +74,7 @@ exports.sendOTP = async(req, res) => {
         await transporter.sendMail(mailOptions);
         res.status(200).json({
             success: true,
-            otp:otp,
+            otp: otp,
             message: 'OTP sent successfully'
         });
     } catch (error) {
