6ecf3ed66c0dcccead1b492c324ca425715287df
neerajsde <neerajprajapatise@gmail.com>
2024-05-25 20:32:12 +0530

    update into changePass

--- controllers/changePass.js
+++ controllers/changePass.js
@@ -1,5 +1,7 @@
 const user = require('../models/signup');
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 exports.changePass = async (req, res) => {
     try {
@@ -26,6 +28,35 @@ exports.changePass = async (req, res) => {
             });
         }
 
+        // Create a transporter object using SMTP transport
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            secure: true,
+            port: 465,
+            auth: {
+                user: process.env.EMAIL_USER, // your Gmail address from environment variable
+                pass: process.env.EMAIL_PASS // your Gmail password from environment variable
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: 'Todo App - Password Changed Successfully',
+            text: `Dear ${findEmail[0].name},
+
+            We are writing to inform you that your password has been successfully changed.
+            
+            If you did not initiate this change, please contact our support team immediately to secure your account.
+            
+            Thank you for your attention to this matter and for using our Todo App.
+            
+            Best regards,
+            The Todo App Team`
+        };
+        
+        console.log("Mail Options:", mailOptions);         
+
         // Update the password
         const updatePassword = await user.findByIdAndUpdate(
             findEmail[0]._id, // Accessing the _id of the first user object
@@ -33,6 +64,7 @@ exports.changePass = async (req, res) => {
             { new: true }
         );
 
+        await transporter.sendMail(mailOptions);
         res.status(200).json({
             success: true,
             user: updatePassword,
